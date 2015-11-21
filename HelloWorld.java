8c053f4c0ec0773a705596b5ac7b5e2a8a3555e4
shen <1932365604@qq.com>
2015-11-21 23:38:04 +0800

    Update HelloWorld.java File. add two lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld
 {
 	public static void main(String[] args){
-		System.out.println("HelloWorld��");
+		System.out.println("========修改之前========");
+		System.out.println("HelloWorld！");
+		System.out.println("========修改之后========");
+
 	}
 }
