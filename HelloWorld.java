26a0d7451e017821fc1fb8d0f9135befaa4e1f76
shen <1932365604@qq.com>
2015-11-22 00:01:02 +0800

    change the 2 file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
-public class HelloWorld
+﻿public class HelloWorld
 {
 	public static void main(String[] args){
 		System.out.println("========修改之前========");
 		System.out.println("HelloWorld！");
 		System.out.println("========修改之后========");
-
+		System.out.println("========再次修改之后========");
+		System.out.println("========再次修改之后========");
 	}
 }
