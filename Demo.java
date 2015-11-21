26b2d15bdbaea47d289ca5c6a985d202277d27af
shen <1932365604@qq.com>
2015-11-22 01:21:14 +0800

    添加两行操作

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ public class HelloWorld
 {
 	public static void main(String[] args){
 		System.out.println("========Demo操作========");
+		System.out.println("========添加一行demo========");
 		System.out.println("========Demo操作========");
+		System.out.println("========添加两行demo========");
 	}
 }
